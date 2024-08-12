b3e04959bf7722722cce12d8a57be3495466cd10
Rudalph <gonsalvesrudalph@gmail.com>
2024-08-12 19:20:12 +0530

    features updated

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Hero from "@/Components/Hero";
 import About from "@/Components/About";
+import Features from "@/Components/Features";
 
 export default function Home() {
   return (
     <>
     <Hero />
     <About />
+    <Features />
     </>
   );
 }
